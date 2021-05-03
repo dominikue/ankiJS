4ba572f8253d6b09329e6221a4e80b55064b4a5f
Dominik <dominik.ueruem@icloud.com>
2021-05-03 13:52:39 +0200

    Added cloze cards

--- ankiJS.js
+++ ankiJS.js
@@ -1,62 +1,135 @@
-var export_txt = "# CSV AnkiJS - Import for deck: ";
+var export_basic = "# BASIC DECK :: CSV AnkiJS - Import for ";
+var export_cloze = "# CLOZE DECK :: CSV AnkiJS - Import for ";
+
 
 function get_all_flashcards() {
 
-    export_txt += $(document).attr('title');
+    export_basic += $(document).attr('title');
+    export_cloze += $(document).attr('title');
     
-    $.each($("blockquote > blockquote > blockquote"), function() {
+    $.each($('body > blockquote > blockquote'), function() {
         
         // Note text var for exporting with new line
         var note_txt = "\n";
+
+        if ($(this).find('blockquote').length) {
+            // -- IF BASIC with question and answer --
+            
+            // QUESTION
+            var q_i = 0;
+            
+            $(this).children('p').each(function() {
+                // FIELD 1: ID
+                if (q_i == 0) {
+                    note_txt += $(this).html() + ';';
+                }
+                // FIELD 2: QUESTION
+                else {
+                    // Check if question has multiple lines (<br>)
+                    if (q_i > 1) {
+                        note_txt += '<br>';
+                    }
+                    
+                    console.log($(this).html());
+                    
+                    note_txt += $(this).html();
+                }
+                q_i = q_i + 1;
+            });
+     
+            // ANSWER
+            note_txt += ";";
+            var a_i = 0;
+            
+            $(this).find('blockquote').children().each(function() {
                 
-        // QUESTION
-        var q_i = 0;
-        
-        $(this).parent().children('p').each(function() {
-            // FIELD 1: ID
-            if (q_i == 0) {
-                note_txt += $(this).html() + ';';
-            }
-            // FIELD 2: QUESTION
-            else {
-                // Check if question has multiple lines (<br>)
-                if (q_i > 1) {
+                // Check if answer has multiple lines (<br>)
+                if (a_i > 0) {
                     note_txt += '<br>';
                 }
                 
-                console.log($(this).html());
+                //if q / ul
+                if ($(this).is('ul')){
+                    var q_txt = '<ul>' + $(this).html() + '</ul>'
+                } else {
+                    var q_txt = $(this).html()
+                }
+                q_txt = q_txt.replace(/(\r\n|\n|\r|\t)/gm, "");
                 
-                note_txt += $(this).html();
-            }
-            q_i = q_i + 1;
-        });
-        
- 
-        // ANSWER
-        note_txt += ";";
-        var a_i = 0;
-        
-        $(this).children('p').each(function() {
+                note_txt += q_txt;
+                
+                a_i = a_i + 1;
+            });
             
-            // Check if answer has multiple lines (<br>)
-            if (a_i > 0) {
-                note_txt += '<br>';
-            }
-            note_txt += $(this).html();
+            // REPLACING Chars
+            console.log('BASIC:');
+            console.log(note_txt);
+            export_basic += note_txt;
             
-            a_i = a_i + 1;
-        });
+        } else {
+            // -- ELSE CLOZE with only question --
+            
+            // QUESTION with CLOZE
+            var q_i = 0;
+            
+            $(this).children().each(function() {
+                // FIELD 1: ID
+                if (q_i == 0) {
+                    note_txt += $(this).html() + ';';
+                }
+                // FIELD 2: QUESTION with cloze
+                else {
+                    // Check if question has multiple lines (<br>)
+                    if (q_i > 1) {
+                        note_txt += '<br>';
+                    }
+                    
+                    //if q / ul
+                    if ($(this).is('ul')){
+                        var q_txt = '<ul>' + $(this).html() + '</ul>'
+                    } else {
+                        var q_txt = $(this).html()
+                    }
+                    q_txt = q_txt.replace(/(\r\n|\n|\r|\t)/gm, "");
+
+                    q_txt = q_txt.replace(/<cite>:[0-9]+ /gi, function (match) {
+                        console.log('MATCH:');
+                        match = match.substring(0, match.length - 1);
+                        match = match.replace('<cite>:', '');
+                        console.log('MATCH:');
+                        console.log(match);
+                        return '{{c' +  match + '::';
+                    });
+
+                    var q_cloze_i = 0;
+                    q_txt = q_txt.replace(/<cite>/gi, function() {
+                        q_cloze_i++;
+                        return '{{c' + q_cloze_i + '::'
+                    });
+                    
+                    q_txt = q_txt.replace('</cite>', '}}');
+                    
+                    console.log(q_txt);
+                    note_txt += q_txt;
+                }
+                q_i = q_i + 1;
+            });
+            
+            console.log('CLOZE:');
+            console.log(note_txt);
+            export_cloze += note_txt;
+        }
         
-        // REPLACING Chars
-        console.log(note_txt);
-        export_txt += note_txt;
+                
+        
     });
     
-    download_txt();
+    download_txt(export_basic, 'BASIC_');
+    download_txt(export_cloze, 'CLOZE_');
 }
 
 
-function  download_txt() {
+function  download_txt(export_text, prefix) {
   // Create an invisible A element
   const a = document.createElement("a");
   a.style.display = "none";
@@ -64,11 +137,11 @@ function  download_txt() {
 
   // Set the HREF to a Blob representation of the data to be downloaded
   a.href = window.URL.createObjectURL(
-    new Blob([export_txt], {type: "text/plain" })
+    new Blob([export_text], {type: "text/plain" })
   );
 
   // Use download attribute to set set desired file name
-  a.setAttribute("download", $(document).attr('title') + '_anki.txt');
+  a.setAttribute("download", prefix + $(document).attr('title') + '_anki.txt');
 
   // Trigger the download by simulating click
   a.click();
@@ -77,6 +150,7 @@ function  download_txt() {
   window.URL.revokeObjectURL(a.href);
   document.body.removeChild(a);
 }
+
 
 $(document).ready(function() {
     document.getElementById('btn_dowload').addEventListener('click', get_all_flashcards);
